ffb52af85ddbeb5acf10da6165b4ca18b722cb23
Thu Tran <tntran@smith.edu>
2021-12-21 01:29:22 +0700

    Create descriptions.py

---
+++ descriptions.py
@@ -0,0 +1,33 @@
+sc1_description = """In Physics we are designing a rocket that will launch into the air. You will apply aerodynamics to build the most efficient rocket, having the lightest mass, while ensuring its stability in the air. Focus on factors like weight, length, width, and fins that are affected by wind. Analyze the rocket’s flight, its velocity over time, height over time, and how long the flight was. In addition, scale your analysis if your rocket is to be launched in space. How far do you think your rocket will launch into outer space, beyond Earth’s atmosphere?"""
+
+sc2_description = """In today’s class, we are going to perform an aspirin synthesis experiment. Aspirin, also called acetylsalicylic acid (ASA), is a synthesized chemical that is often used to treat pain, fever, and inflammatory diseases. For the synthesis, we will combine salicylic acid and acetic anhydride to produce aspirin with a byproduct acetic acid in the presence of an acid catalyst such as sulfuric acid. We will then go through the purification process by recrystallizing the solid aspirin and compare the purity of crude and recrystallized aspirin using the melting point and the concentration of one of the impurities, salicylic acid.
+
+We will use visual spectroscopy to find the concentration of the salicylic acid using the idea that its color change when mixed with the iron stock solution. We will measure the absorbance value of the different concentrations of salicylic acid and make the calibration plot using those data points. The Beer's law will be used to calculate the molar absorptivity of salicylic acid, which would be represented by the slope of the linear graph. Stoichiometry will be applied. """
+
+
+sc3_description = """In this lab, we will dissect a preserved cow eye which resembles very closely to human eyes. Both contain similar structures such as the sclera and cornea. We will cut away thick layers of fat and muscle tissues to examine the sclera and optic nerve, mainly using a scalpel to make physical slits on the eye. This will inform us how our eyes form images (including light reflection, color perception, and vision) to ultimately transmit signals to our brain. """
+
+sc4_description = """In this class, we will study some algorithms in computational approaches. Python is a high-level programming language that allows humans to communicate with the computer. We will now learn about several functions that make you be able to code!  
+
+1. Print() function prints the content inside the parentheses to the screen. 
+Ex) if input is the following: print(“Hello World”), output on the console will be the following: Hello World
+2. Input() function allows the interaction of the program with the user to type in contents. 
+Ex) if input is the following: x = input("What color do you like?"), output on the console: What color do you like? (a space to type)
+3. if/else statement allows to make a conditional situation that shows different results for different scenarios. 
+Ex) if input is the following: if x == “yellow”: print(“You like yellow!”) else: print(“You don’t like yellow!”), the output prints 'You like yellow!' when the user types yellow, while it prints 'You don't like yellow!' when the user types anything other than yellow."""
+
+sc5_description = """More than ever, the Earth is experiencing a rapid increase in climate change due to human activity. Climate change significantly impacts the ecosystem and all the living organisms in it, altering organisms’ habitats, food chains, and all sorts of interactions. The 6 factors that affect climate change includes the following: latitude, ocean currents, wind, elevation, relief, and nearness to the ocean. Some measures of the climates include temperature, rainfall, and wind. In this class, we will focus on learning about the negative impacts of climate change on Earth and some ways to address the existing problems."""
+
+sc6_description = """In this lab, we will understand and conduct a sheep brain dissection. Before we get started, we must research the properties of the sheep brain, like the neurons of the cerebral cortex. We can study its distribution of the cells in the brain to grasp a better understanding. The current molecular basis of nerve cell death in sheep is not well understood, so the dissection will come in hand to cut through the brain and find the white matter, or inner portion of the cerebrum."""
+
+sc7_description = """In today’s class, we will calculate the optimal angles of windows in a house to allow maximum sunlight to enter. A strong background of trigonometry is necessary to calculate sine, cosine, and tangent. This will help construct and determine where the solar panels are placed on the roof of the house in our blueprint diagram. The skills to analyze the house structure and its architecture is useful. """
+
+sc8_description = """Gathering and interpreting the quantitative data using the scientific method is crucial in developing the scientific field in our society. Today, we will use basic coding and statistics skills to effectively analyze the data. Linear regression is one of the techniques to organize the data. From the scatter plot, the linear regression line can be drawn as a line of best fit, and the coefficient of determination (R2) shows the strength of the correlation, as well as the slope of the equation."""
+
+sc9_description = """Today, we will have a field trip to a garden and choose one plant to measure its behavior. We will compare the control group and the experimental group of the same plant that are given the same condition except the difference of sunlight intake by keeping one under the sunlight and covering the other with a black panel around it. We will measure the growth of the plant over the period of 10 days, and analyze the data using graphs and statistics."""
+
+sc10_description = """Statistical Physics uses probability theory and statistics to solve physical problems. Here is a sample of a problem that focuses on thermodynamics (see picture above): """
+
+wrong_key_message = "\nCould not identify this position. Press one of the following options: Left, Right, or Down"
+
+confirmation_key_message = "\nPlease confirm the key you pressed by pressing it again, or if you would like to switch, press the new key now."
