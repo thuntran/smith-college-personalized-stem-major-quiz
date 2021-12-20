b6eff048a3c2ca6e5f4ad7204c9a525a5d8a783b
Thu Tran <tntran@smith.edu>
2021-12-21 01:30:52 +0700

    Create scenarios.py

---
+++ scenarios.py
@@ -0,0 +1,134 @@
+# Import descriptions
+from descriptions import * 
+
+# Scenario 1 
+class Scenario1():
+  def __init__(self):
+    """Create Scenario 1 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""  
+    
+    self.title = "BUILD A ROCKET"
+    self.description = sc1_description
+    self.question = "Do you like this idea?" 
+    self.like = "Yes, I love it\n[Press Left]" 
+    self.neutral = "Sounds okay\n[Press Down]" 
+    self.dislike = "Nah, uninterested\n[Press Right]" 
+    self.name = "SCENARIO 1"
+
+
+# Scenario 2 
+class Scenario2():
+  def __init__(self):
+    """Create Scenario 2 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "ASPIRIN SYNTHESIS LAB"
+    self.description = sc2_description
+    self.question = "Do you like this idea?"
+    self.like = "It’s fun and intriguing\n[Press Left]" 
+    self.neutral = "OK, but not my favorite\n[Press Down]" 
+    self.dislike = "Waste of time\n[Press Right]" 
+    self.name = "SCENARIO 2"
+    
+# Scenario 3
+class Scenario3():
+  def __init__(self):
+    """Create Scenario 3 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "COW EYE DISSECTION"
+    self.description = sc3_description
+    self.question = "Was that super interesting, ok, or disgusting?"
+    self.like = "Super interesting\n[Press Left]" 
+    self.neutral = "It was okay\n[Press Down]" 
+    self.dislike = "Disgusting\n[Press Right]"
+    self.name = "SCENARIO 3"
+
+# Scenario 4
+class Scenario4():
+  def __init__(self):
+    """Create Scenario 4 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "CODING IN PYTHON"
+    self.description = sc4_description
+    self.question = "How was this activity?"
+    self.like = "Fun and intriguing\n[Press Left]" 
+    self.neutral = "Manageable\n[Press Down]" 
+    self.dislike = "Waste of time\n[Press Right]" 
+    self.name = "SCENARIO 4"
+    
+
+# Scenario 5
+class Scenario5():
+  def __init__(self):
+    """Create Scenario 5 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "CLIMATE CHANGE"
+    self.description = sc5_description
+    self.question = "Was that super interesting, ok, or disgusting?"
+    self.like = "Yes, I love it\n[Press Left]" 
+    self.neutral = "Sounds okay\n[Press Down]" 
+    self.dislike = "Nah, uninterested\n[Press Right]"
+    self.name = "SCENARIO 5"
+
+# Scenario 6
+class Scenario6():
+  def __init__(self):
+    """Create Scenario 6 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "CONDUCT A SHEEP BRAIN DISSECTION"
+    self.description = sc6_description
+    self.question = "Do you like this idea?"
+    self.like = "Super interesting\n[Press Left]" 
+    self.neutral = "It was okay\n[Press Down]" 
+    self.dislike = "Disgusting\n[Press Right]"
+    self.name = "SCENARIO 6"
+
+# Scenario 7
+class Scenario7():
+  def __init__(self):
+    """Create Scenario 7 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "SOLAR PANELS"
+    self.description = sc7_description
+    self.question = "Do you like this idea?"
+    self.like = "Yes, I absolutely love it!\n[Press Left]" 
+    self.neutral = "Sounds okay\n[Press Down]" 
+    self.dislike = "Nah, uninterested\n[Press Right]" 
+    self.name = "SCENARIO 7"
+
+# Scenario 8
+class Scenario8():
+  def __init__(self):
+    """Create Scenario 8 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "DATA SCIENCE IN PYTHON"
+    self.description = sc8_description
+    self.question = "Do you like this idea?"
+    self.like = "Yes, I love it\n[Press Left]" 
+    self.neutral = "Sounds okay\n[Press Down]" 
+    self.dislike = "Nah, uninterested\n[Press Right]" 
+    self.name = "SCENARIO 8"
+
+# Scenario 9
+class Scenario9():
+  def __init__(self):
+    """Create Scenario 9 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "MEASURING PLANTS IN THE GARDEN"
+    self.description = sc9_description
+    self.question = "Do you like this idea?"
+    self.like = "Yes, I love it\n[Press Left]" 
+    self.neutral = "Sounds okay\n[Press Down]" 
+    self.dislike = "Nah, uninterested\n[Press Right]" 
+    self.name = "SCENARIO 9"
+
+# Scenario 10
+class Scenario10():
+  def __init__(self):
+    """Create Scenario 10 with its corresponding information: title, description, question, options (LIKE, NEUTRAL, DISLIKE), name"""
+
+    self.title = "STATISTICAL PHYSICS"
+    self.description = sc10_description
+    self.question = "Is this something that you are interested in learning?"
+    self.like = "Yes, this is for me!\n[Press Left]" 
+    self.neutral = "It is okay\n[Press Down]" 
+    self.dislike = "No, this is not for me\n[Press Right]" 
+    self.name = "SCENARIO 10"
